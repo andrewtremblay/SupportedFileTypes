255c60a3350e71da02bf0825c557178cfa510f23
Andrew Tremblay <gloriousrocketship@gmail.com>
2013-11-09 21:03:12 -0800

    adding more files and content

---
+++ MYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MYAppDelegate.h
+//  EmptyTestProject
+//
+//  Created by AndrewTremblay on 11/9/13.
+//  Copyright (c) 2013 plsIgnore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
